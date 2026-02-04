928693802256bb3ae8845c29b1fb897f00a19ad9
ALUNO SENAI PETROLINA <aluno.senaipetrolina@SISTEMAFIEPE.LOCAL>
2026-02-04 10:28:32 -0300

    feat: protege rota de criação demposts

--- routes/routes.js
+++ routes/routes.js
@@ -3,13 +3,14 @@ const router = express.Router()
 
 const authController = require("../controller/authController")
 const postController = require("../controller/postController")
+const authMiddleware = require("../middlewares/authMiddleware")
 
 //Usuario
 router.post("/usuarios",authController.criarUsuario)
 router.post("/login",authController.login)
 
 //Posts
-router.post("/posts", postController.criarPost)
+router.post("/posts", authMiddleware, postController.criarPost)
 router.get("/posts", postController.listarPosts)
 
 module.exports = router
